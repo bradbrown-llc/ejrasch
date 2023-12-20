806e22cef8869817712cbcc93e5cc90b21d6b618
Brad Brown <bradbrown@bradbrown.llc>
2023-12-19 20:47:48 -0500

    orNull!pend

--- schs/uncleByBlockHashAndIndex.ts
+++ schs/uncleByBlockHashAndIndex.ts
@@ -1,9 +1,2 @@
-import pendingBlock from '../_lib/pendingBlock.ts'
 import block from '../_lib/block.ts'
-export default {
-    any: [
-        { type: 'null' },
-        block,
-        pendingBlock
-    ]
-} satisfies Sch
+export default { any: [{ type: 'null' }, block] } satisfies Sch
